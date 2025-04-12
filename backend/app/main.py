ca07e8bc3dd86f5662348098da585db5fe6501d7
Ritvik MNS <naga.sai@research.iiit.ac.in>
2025-04-12 19:47:19 +0530

    some changes

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,23 @@
 from fastapi import FastAPI
+from fastapi.routing import APIRouter
 from strawberry.fastapi import GraphQLRouter
 import strawberry
 
 @strawberry.type
 class Query:
-    hello: str = "Welcome to Smart Canteen API"
+    hello: str = "Hello from GraphQL!"
 
 graphql_schema = strawberry.Schema(query=Query)
-graphql_app = GraphQLRouter(graphql_schema)
+graphql_app = GraphQLRouter(schema=graphql_schema)
 
 app = FastAPI()
-app.include_router(graphql_app, prefix="/graphql")
+
+# GraphQL endpoint
+api_router = APIRouter()
+api_router.include_router(graphql_app, prefix="/graphql")
+app.include_router(api_router, prefix="/api")
+
+# REST endpoint
+@app.get("/api/")
+async def read_root():
+    return {"message": "Hello from REST!"}
